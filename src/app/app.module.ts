a5825b2422a6d65d787cae03d151d4befbef2d84
akila <akilagodigamuwa@gmail.com>
2023-10-14 22:01:18 +0530

    Added footer and assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
